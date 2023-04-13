012cdfb3ab9a0d4b0a92282e74de83e0348ca05c
Rogério Soares <rgrsoares@yahoo.com.br>
2023-04-13 15:37:22 -0600

    Correção dos bugs
    Agora funciona bem!

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,35 +1,32 @@
 const monthEl = document.querySelector(".date h1")
 const fullDateEl = document.querySelector(".date p")
-const datesEl = document.querySelector(".date")
+const datesEl = document.querySelector(".dates")
 
 const monthIndex = new Date().getMonth()
 const lastDate = new Date(new Date().getFullYear(), monthIndex + 1, 0).getDate()
 const firstDate = new Date(new Date().getFullYear(), monthIndex,1).getDay()
 
-const months=["Janeiro", "Fevereiro", "Março",  "Abril",  "Maio",
-"Junho","Julho","Agosto", "Setembro",
-"Outubro", "Novembro", "Dezembro"]
+const months = ["Janeiro","Fevereiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro","Dezembro"]
 
-const days=["Domingo","Segunda","Terça",
-"Quarta","Quinta","Sexta","Sábado"]
+const days = ["Domingo","Segunda","Terça","Quarta","Quinta","Sexta","Sábado"]
 
 monthEl.innerText= months[monthIndex]
 
-const newDate=new Date()
+const newDate = new Date()
 
-fullDateEl.innerText= days[newDate.getDay()] + ", " + newDate.getDate() + " " + months[newDate.getMonth()] + " " + newDate.getFullYear();
+fullDateEl.innerText = days[newDate.getDay()] + ", " + newDate.getDate() + " " + months[newDate.getMonth()] + " " + newDate.getFullYear();
 
-let dates="";
+let dates = "";
 
-for(i=firstDate; i>0; i--){
+for(let i = firstDate; i > 0; i--){
     dates += `<div class="empty"></div>`
 }
 
-for(i=l; i<=lastDate; i++){
-    if(i === new Date().getDate()){
-        dates += `<div class="today">${i}</div>`
+for(let j = 1; j <= lastDate; j++){
+    if(j === new Date().getDate()){
+        dates += `<div class="today">${j}</div>`
     }else{
-        dates += `<div>${i}</div>`
+        dates += `<div>${j}</div>`
     }
 }
 
